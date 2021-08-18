da0615a198c4a4b74585622edf227189c690c3f9
fullWaveRectifier <88315678+fullWaveRectifier@users.noreply.github.com>
2021-08-18 23:32:05 +0530

    Added UserHomePage

--- src/app/user-home-page/user-home-page.component.ts
+++ src/app/user-home-page/user-home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Coach } from '../models/coach.model';
+import { CoachService } from '../services/coach.service';
 
 @Component({
   selector: 'app-user-home-page',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-home-page.component.css']
 })
 export class UserHomePageComponent implements OnInit {
+  coaches: Coach[] = [new Coach("sfsdf", "efsdf", "dfedf", "sdf", "sdf", 2323, "asdf")];
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private router:Router, private coachservice: CoachService) { }
 
   ngOnInit(): void {
+    this.getAllCoaches();
+
+  }
+
+  getAllCoaches() {
+     this.coachservice.getAllCoaches().subscribe((result)=>{
+       console.log(result)
+       this.coaches = result;
+     })
   }
 
 }
